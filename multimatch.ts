16fbc2a62aecfbef3772be1992139aee3bbfed81
Cyprien Autexier <cyprien.autexier@gmail.com>
2016-08-08 14:52:15 +0200

    fix multimatch negative test regression

--- multimatch.ts
+++ multimatch.ts
@@ -1,10 +1,21 @@
 import micromatch = require('micromatch');
-import tl = require('vsts-task-lib/task');
 import path = require('path');
+import tl = require('vsts-task-lib/task');
 
 export function getMatches(directory: string, filters: string) : string[] {
-    var patterns = filters.split("\n").map((pattern) => path.join(directory, pattern));
-    var allFiles = tl.find(directory).map(file => path.resolve(file));
-    var files = allFiles.filter(micromatch.filter(patterns, { nodupes: true }));
-    return files;
+    return applyMatch(directory, filters, tl.find(directory));
+}
+
+export function applyMatch(directory: string, filters: string, files: string[]){
+      var patterns = filters.split("\n").map((pattern) => {
+        if (pattern.match(/^!/)) {
+            return '!' + path.join(directory, pattern.substr(1));
+        }
+        return path.join(directory, pattern)
+    });
+
+    var allFiles = files.map(file => path.resolve(file));
+    var filteredFiles = allFiles.filter(micromatch.filter(patterns, { nodupes: true }));
+    
+    return filteredFiles;
 }
